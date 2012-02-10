dc95f5103a84cc26f10b00a2d223f9b2a422edb9
unknown <Javieku@Javieku-PC.(none)>
2012-02-10 17:03:27 +0100

    Proyecto listo para subirse a appWorld

---
+++ Zarodnik/src/org/example/activities/SettingsActivity.java
@@ -0,0 +1,90 @@
+package org.example.activities;
+
+import org.example.R;
+
+import org.example.tinyEngineClasses.TTS;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class SettingsActivity extends PreferenceActivity implements
+		OnPreferenceClickListener {
+
+	private CheckBoxPreference music, profileA, profileB;
+
+	// Option names and default values
+	private static final String OPT_MUSIC = "music";
+	private static final boolean OPT_MUSIC_DEF = false;
+	private static final String OPT_PROFILEA = "profile A";
+	private static final String OPT_PROFILEB = "profile B";
+	private TTS textToSpeech;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+
+		// Get the checkbox preference
+		music = (CheckBoxPreference) findPreference(OPT_MUSIC);
+		music.setOnPreferenceClickListener(this);
+
+		profileA = (CheckBoxPreference) findPreference(OPT_PROFILEA);
+		profileA.setOnPreferenceClickListener(this);
+		profileB = (CheckBoxPreference) findPreference(OPT_PROFILEB);
+		profileB.setOnPreferenceClickListener(this);
+		
+
+		// Initialize TTS engine
+		textToSpeech = (TTS) getIntent().getParcelableExtra(MainActivity.KEY_TTS);
+		textToSpeech.setContext(this);
+		textToSpeech.setInitialSpeech("Click any option");
+	}
+
+	/**
+	 * Turns off TTS engine
+	 */
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		textToSpeech.stop();
+	}
+
+	/** Get the current value of the music option */
+	public static boolean getMusic(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context)
+				.getBoolean(OPT_MUSIC, OPT_MUSIC_DEF);
+	}
+
+	public boolean onPreferenceClick(Preference preference) {
+		if (OPT_MUSIC.equals(preference.getKey())) {
+			textToSpeech.speak(findPreference(OPT_MUSIC).toString()
+					+ music.isChecked());
+		} else if (OPT_PROFILEA.equals(preference.getKey())) {
+			textToSpeech.speak(findPreference(OPT_PROFILEA).toString()
+					+ profileA.isChecked());
+			manageProfileA();
+		} else if (OPT_PROFILEB.equals(preference.getKey())) {
+			textToSpeech.speak(findPreference(OPT_PROFILEB).toString()
+					+ profileB.isChecked());
+			manageProfileB();
+		}
+		return true;
+	}
+
+	private void manageProfileA() {
+		profileB.setChecked(false);;
+
+	}
+	
+	private void manageProfileB() {
+
+		 profileA.setChecked(false);
+
+	}
+	
+}
---
+++ Zarodnik/src/org/example/tinyEngineClasses/GameState.java
@@ -0,0 +1,258 @@
+package org.example.tinyEngineClasses;
+
+import java.util.ArrayList;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.example.others.RuntimeConfig;
+import org.example.tinyEngineClasses.Input.EventType;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+
+
+public abstract class GameState {
+	
+	public static int SCREEN_WIDTH;
+	public static int SCREEN_HEIGHT;
+	public static float scale;
+	
+	protected View v;
+	
+	protected Activity context;
+	
+	private Bitmap background = null;
+	
+    boolean game_is_running;
+	
+	private List<Entity> entities;
+	private List<Entity> collidables;
+	private List<Entity> renderables;
+	private List<Entity> removables;
+
+	private TTS textToSpeech;
+
+	private Paint brush;
+	
+	protected Game game;
+	private boolean onInitialized;
+	
+	public GameState(View v, Context context, TTS textToSpeech, Game game){
+		game_is_running = true;
+		this.game = game;
+		this.context = (Activity) context;
+		this.v = v;
+		this.setTextToSpeech(textToSpeech);
+		entities = new ArrayList<Entity>();
+		collidables = new ArrayList<Entity>();
+		renderables = new ArrayList<Entity>();
+		removables = new ArrayList<Entity>();
+		brush = null;
+		
+		Display display = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+		SCREEN_WIDTH = display.getWidth();
+		SCREEN_HEIGHT = display.getHeight();
+		// Get the screen's density scale
+		scale = context.getResources().getDisplayMetrics().density;
+		
+		onInitialized = false;
+	}
+	
+	public void onInit() {
+		Iterator<Entity> it = entities.iterator();
+		Entity e;
+		while(it.hasNext()){
+			e = it.next();
+			e.onInit();
+		}
+		onInitialized = true;
+	}
+	
+	protected void onDraw(Canvas canvas) {
+		if(background != null)
+			canvas.drawBitmap(background,0,0,brush);
+		
+		Iterator<Entity> it = renderables.iterator();
+		Entity e;
+		while(it.hasNext()){
+			e = it.next();
+			e.onDraw(canvas);
+		}
+		
+		renderables.clear();
+	}
+	
+	protected void onUpdate(){
+		Entity e1,e2,e = null;
+		Iterator<Entity> it = entities.iterator();
+		while(it.hasNext()){
+			e = it.next();
+			if(e.isCollidable() && e.isEnabled())
+				collidables.add(e);
+			if(e.isRenderable() && e.isEnabled())
+				renderables.add(e);
+			if(e.isRemovable())
+				removables.add(e);
+			if(!e.isFrozen()){
+				e.onUpdate();
+			}
+		}
+		
+		Iterator<Entity> it1 = collidables.iterator();
+		Iterator<Entity> it2;
+		while(it1.hasNext()){
+			e1 = it1.next();
+			it2 = collidables.iterator();
+			while(it2.hasNext()){
+				e2 = it2.next();
+				if (e1 != e2 & e1.collides(e2)) 
+					e1.onCollision(e2);
+			}
+			
+		}
+		
+		it  = removables.iterator();
+		while(it.hasNext()){
+			e = it.next();
+			e.onRemove();
+			entities.remove(e);
+			collidables.remove(e);
+			renderables.remove(e);
+			e.delete();
+		}
+		
+		collidables.clear();
+		removables.clear();
+		
+		EventType event = Input.getInput().removeEvent("onKeySearch");
+		if(event != null){
+			RuntimeConfig.IS_DEBUG_MODE = !RuntimeConfig.IS_DEBUG_MODE;
+		}
+	}
+	
+	public void run(){
+		game_is_running = true;
+	}
+	
+	public void stop(){
+		game_is_running = false;
+	}
+	
+	public boolean isRunning(){
+		return game_is_running;
+	}
+	
+	public void addEntity(Entity e){	
+		entities.add(e);
+	}
+	
+	public void removeEntity(Entity e){	
+		removables.add(e);
+	}
+	
+	public View getView(){
+		return v;
+	}
+	
+	public Activity getContext(){
+		return context;
+	}
+	
+	public Bitmap getBackground(){
+		return background;
+	}
+	
+	public List<Entity> getEntities(){
+		return entities;
+	}
+	
+	public List<Entity> getRenderables(){
+		return renderables;
+	}
+	
+	
+	/**
+	 * if the previous background is not freed this method recycled it.
+	 * 
+	 * */
+	protected void setBackground(Bitmap background) {
+		if(this.background != null)
+			this.background.recycle();
+		this.background = background;
+	}
+	
+	public TTS getTTS(){
+		return getTextToSpeech();
+	}
+	
+	public void editBackground(Paint brush){
+		this.brush = brush;
+	}
+
+	public TTS getTextToSpeech() {
+		return textToSpeech;
+	}
+
+	public void setTextToSpeech(TTS textToSpeech) {
+		this.textToSpeech = textToSpeech;
+	}
+	
+	
+	public boolean positionFreeEntities(Entity e1){
+		Iterator<Entity> it = entities.iterator();
+		Iterator<Mask> i;
+		boolean empty = true;
+		Entity e; Mask m; int w,h;
+		while (empty && it.hasNext()){
+			e = it.next();
+			if (e.isCollidable()){
+				i = e.getMask().iterator();
+				while (empty && i.hasNext()){
+					m = i.next();
+					if (m instanceof MaskBox){
+						w = ((MaskBox)m).getWidth();
+						h = ((MaskBox)m).getHeight();
+					} else{
+						w = h = ((MaskCircle)m).getRadius() * 2;
+					}
+						
+					empty = !encloses(w, h, e1, e.getX(),e.getY());
+				}
+			}
+		}
+		return empty;
+	}
+
+	private boolean encloses(int w1, int h1, Entity e, float ex, float ey) {
+		// para cada máscara de e
+		Iterator<Mask> i = e.getMask().iterator();
+		boolean ok = true; Mask m;
+		int w, h;
+		while (ok && i.hasNext()){
+			m = i.next();
+			if (m instanceof MaskBox){
+				w = ((MaskBox)m).getWidth();
+				h = ((MaskBox)m).getHeight();
+			} else{
+				w = h = ((MaskCircle)m).getRadius() * 2;
+			}
+			if ((ey <= e.getY() + h) && (ey + h1 >= e.getY()))			// width match
+				if ((ex <= e.getX() + w) && (ex + w1 >= e.getX()))		// heigh match
+					ok = false;
+			
+		}
+
+		return !ok;
+	}
+
+	public boolean isOnInitialized() {
+		return onInitialized;
+	}
+}
